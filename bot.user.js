f44ef025919a4f24d58a980e88b5bddd7ea947ad
awesomemc2 <edisondieu22@gmail.com>
2016-07-01 15:03:30 -0700

    Create bot.user.js
    copy this

---
+++ bot.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         bot animal jam v1
+// @namespace    bot animal jam v1
+// @version      0.1
+// @description  animal jam bot v1
+// @author       AJHACKER1
+// @match        http://www.animaljam.com/game/play
+// @grant        none
+// ==/UserScript==
+
+$(function() {
+    'bot';
+
+   // start-bot
+   if connnect to player
+    connect.user.player($(function("FINDAPERSON")
+})();
+
+// wifi
+   if wifi connnect to player
+    connect.wifi.user.player($(function("wififinder1")
+})();
